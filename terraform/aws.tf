ad2071793eaf6f99c01d34130e02cfab77e1c432
koga.shinya <shinya4study@gmail.com>
2026-06-02 15:15:22 +0900

    test

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,7 +1,7 @@
 # S3バケット
 resource "aws_s3_bucket" "bronze" {
   bucket = "${var.project_name}-bronze"
-  tags = { Environment = "dev" }
+  tags   = { Environment = "dev" }
 }
 
 resource "aws_s3_bucket" "silver" {
